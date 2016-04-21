76b5443bef06753d605c7ff34d1c4aad7ce281c5
martavasconcelos <martavasconcelos14@gmail.com>
2016-04-21 10:03:28 +0100

    apaguem o js geral2

--- meta3/js/geral.js
+++ meta3/js/geral.js
@@ -1,20 +1,19 @@
-var clicks=0;
+var clicks=sessionStorage.getItem("click");
 $("body").on("click", function(){
     clicks++;
     $("#contador").text('Clicks: ' + clicks);
-     sessionStorage . setItem (" c ", clicks);
-    console.log("clicks: " + clicks);
+     sessionStorage.setItem ("click", clicks);
+     console.log("clicks: " + clicks);
 });
 
-var seconds=0;
+var seconds=sessionStorage.getItem("seg");
 var timer = setInterval(
     function() {
         seconds++;
-        sessionStorage . setItem (" seg ", seconds);
+        sessionStorage.setItem("seg", seconds);
         console.log(seconds);
     }, 1000
 );
-
-sessionStorage . removeItem (" seg ");
-sessionStorage . removeItem (" c");
-sessionStorage . clear ();
+sessionStorage.removeItem("seg");
+sessionStorage.removeItem("c");
+sessionStorage.clear();
--- meta3/js/nivel_um.js
+++ meta3/js/nivel_um.js
@@ -1,9 +1,10 @@
 var clicks=0;
-$("body").on("click", function(){
-    clicks++;
-    $("#contador").text('clicks:' + clicks);
-});
+var seconds=0;
+sessionStorage.setItem ("click", clicks);
+sessionStorage.setItem ("seg", seconds);
+
 $("#bambu").on("click", function(){
     $(this).load("nivel_dois.html");
     window.open("nivel_dois.html","_self");
 });
+
