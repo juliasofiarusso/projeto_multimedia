6139fe5cf8cca09352d1e6029cc0ca021868b0b7
martavasconcelos <martavasconcelos14@gmail.com>
2016-04-30 17:36:55 +0100

    nivel lanterna

--- meta3/js/nivel_cinco.js
+++ meta3/js/nivel_cinco.js
@@ -1,9 +1,14 @@
-console.clear();
-
-$( "#overlay" ).mousemove(function( event ) {
- $("#circle").draggable({
-  opacity: 0.35
+$(document).ready(function(){
+  var $moveable = $('#circle');
+  $(document).mousemove(function(e){
+      $moveable.css({'top': e.pageY-100,'left': e.pageX-100});
+  });
 });
+
+
+
+$("#bambu").on("click", function(){
+    console.log("clicked");
+    $(this).load("nivel_seis.html");
+    window.open("nivel_seis.html","_self");
 });
-
-
