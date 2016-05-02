6879092f69ec25ace57aca38a0a45c4b1fb20523
martavasconcelos <martavasconcelos14@gmail.com>
2016-05-02 23:41:06 +0100

    nivel cinco

--- meta3/js/nivel_cinco.js
+++ meta3/js/nivel_cinco.js
@@ -1,16 +1,22 @@
-/*
+
 $(document).ready(function(){
   var $moveable = $('#circle');
   $(document).mousemove(function(e){
       $moveable.css({'top': e.pageY-100,'left': e.pageX-100});
   });
 });
-*/
+
+
+
+
+/*
 var visible = true;
 setInterval(function () {
     document.querySelector('#circle').style.opacity = visible ? 0 : 1;
     visible = !visible;
 }, 550);
+*/
+
 
 
 $("#bambu").on("click", function(){
