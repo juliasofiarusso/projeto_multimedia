adc32fa028d5e9d4183e323a4512a4cd0b24a11e
martavasconcelos <martavasconcelos14@gmail.com>
2016-05-05 12:23:56 +0100

    nivel quinze aceitavel

--- meta3/js/nivel_quinze.js
+++ meta3/js/nivel_quinze.js
@@ -1,8 +1,7 @@
-  var jogo=0; // "pontos"
-    var seq=1; 
-    var jogar=0; //comecar o jogo
+    var seq =1; 
+    var jogar =0; //comecar o jogo
 $(document).ready(function(){
-  
+  if(jogar==0){
     
     setTimeout(
         function(){
@@ -47,19 +46,53 @@ $("#bambu3").animate({opacity: "1"});
             jogar=1;
             console.log("jogar" + jogar);
         },5000);
+  }
     
-setTimeout(
-if(jogar==1){
+//if(jogar==1){
+  //  console.log("jogarcheck");
+    //if(seq==1){
+      //   console.log("seqcheck");
 $("#bambu2").on("click", function(){
-    if(seq==1){
-     jogo++;
+$(this).animate({opacity: "0"}); 
+$(this).animate({opacity: "1"}); 
+    if(jogar==1){
+          if(seq==1){
     seq=2;
+          }
+        else{
+ $("#texto").text("Tenta outra vez!")  
+ jogar=0;
+        }
     }
-  
+    });  
+    
+    $("#bambu").on("click", function(){
+$(this).animate({opacity: "0"}); 
+$(this).animate({opacity: "1"}); 
+    if(jogar==1){
+          if(seq==2){
+    seq=3;
+          }
+        else{
+ $("#texto").text("Tenta outra vez!")  
+ jogar=0;
+        }
+    }
+    }); 
+        $("#bambu3").on("click", function(){
+$(this).animate({opacity: "0"}); 
+$(this).animate({opacity: "1"}); 
+    if(jogar==1){
+          if(seq==3){
+         $("#texto").text("Ganhaste!")
+          }
+        else{
+ $("#texto").text("Tenta outra vez!")  
+ jogar=0;
+        }
+    }
+    });
+    
+   
+//}}
 });
-    
-    
-},6000);  
-    
-      
-} );
