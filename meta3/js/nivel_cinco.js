cd4828137e53cb3a1b2e13ce3083c3858a975652
martavasconcelos <martavasconcelos14@gmail.com>
2016-05-05 15:33:06 +0100

    oi pinxexa 2

--- meta3/js/nivel_cinco.js
+++ meta3/js/nivel_cinco.js
@@ -1,8 +1,12 @@
 
 $(document).ready(function(){
   var $moveable = $('#circle');
+    var $preto =$("overlay");
   $(document).mousemove(function(e){
       $moveable.css({'top': e.pageY-100,'left': e.pageX-100});
+  });
+  $(document).mousemove(function(e){
+      $preto.css({"background": "-webkit-radial-gradient(transparent 100px, rgba(0,0,0,1) 20px);" });
   });
 });
 
--- meta3/js/nivel_quinze.js
+++ meta3/js/nivel_quinze.js
@@ -1,8 +1,10 @@
     var seq =1; 
-    var jogar =0; //comecar o jogo
-$(document).ready(function(){
-  if(jogar==0){
+
+$(document).ready(function (){
     
+    inicio();
+});
+  function inicio(){
     setTimeout(
         function(){
 $("#bambu2").animate({opacity: "1"});         
@@ -43,56 +45,52 @@ $("#bambu").animate({opacity: "1"});
 $("#bambu2").animate({opacity: "1"});  
 $("#bambu3").animate({opacity: "1"});  
             $("#texto").text("Agora é a tua vez!")
-            jogar=1;
-            console.log("jogar" + jogar);
+            jogo();
         },5000);
+    
   }
     
-//if(jogar==1){
-  //  console.log("jogarcheck");
-    //if(seq==1){
-      //   console.log("seqcheck");
-$("#bambu2").on("click", function(){
+  function jogo(){
+          console.log("funcao");
+          $("#bambu2").on("click", function(){
+    $(this).animate({opacity: "0"}); 
+    $(this).animate({opacity: "1"}); 
+              if(seq==1){
+        seq=2;
+              }
+            else{
+     $("#texto").text("Tenta outra vez!")  
+     inicio();
+            }
+            }); 
+      
+            $("#bambu").on("click", function(){
+    $(this).animate({opacity: "0"}); 
+    $(this).animate({opacity: "1"}); 
+              if(seq==2){
+        seq=3;
+              }
+           else if(seq==4){
+      $("#texto").text("Ganhaste!")
+              }
+            else{
+     $("#texto").text("Tenta outra vez!")  
+      inicio();
+            }
+        }); 
+      $("#bambu3").on("click", function(){
 $(this).animate({opacity: "0"}); 
 $(this).animate({opacity: "1"}); 
-    if(jogar==1){
-          if(seq==1){
-    seq=2;
-          }
-        else{
- $("#texto").text("Tenta outra vez!")  
- jogar=0;
-        }
-    }
-    });  
-    
-    $("#bambu").on("click", function(){
-$(this).animate({opacity: "0"}); 
-$(this).animate({opacity: "1"}); 
-    if(jogar==1){
-          if(seq==2){
-    seq=3;
-          }
-        else{
- $("#texto").text("Tenta outra vez!")  
- jogar=0;
-        }
-    }
-    }); 
-        $("#bambu3").on("click", function(){
-$(this).animate({opacity: "0"}); 
-$(this).animate({opacity: "1"}); 
-    if(jogar==1){
           if(seq==3){
-         $("#texto").text("Ganhaste!")
+seq=4;
           }
         else{
  $("#texto").text("Tenta outra vez!")  
- jogar=0;
+  inicio();
         }
-    }
-    });
     
-   
+    });
+
+  }
+
 //}}
-});
