c8b72a001a12c10dfd2fbb2475120a13c5504beb
martavasconcelos <martavasconcelos14@gmail.com>
2016-05-19 22:51:11 +0100

    18

--- meta3/js/nivel_tres.js
+++ meta3/js/nivel_tres.js
@@ -1,26 +1,20 @@
 var clicks=0;
 var seconds=0;
-var top=15;
+var inc=15;
 sessionStorage.setItem ("click", clicks);
 sessionStorage.setItem ("seg", seconds);
 
 $(document).ready(function(){
     
-$("#panda").click(function(){
-    $(init);
+$("#panda").on("click", function(){
+      inc+=4;
     
-    function init() {
-    $("#bambu").css({"top": top++});
-    console.log("panda");
-    
-  /*  $("#bambu").animate({top: top++});
-    console.log("panda");*/
-});     
-    
-});
-
-
-$("#bambu").on("click", function(){
+if(inc==55){
     $(this).load("nivel_quatro.html");
     window.open("nivel_quatro.html","_self");
+}
+     console.log("TOP:" +inc);
+    $('#bambu').animate({ marginTop: inc+"vh" });
+    });
+
 });
