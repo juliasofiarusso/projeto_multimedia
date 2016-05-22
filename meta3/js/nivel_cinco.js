ef1f495e45dd6a6971262fff00be5fc5ad46b27b
juliasofiarusso <julia_russo_@hotmail.com>
2016-05-22 17:35:12 +0100

    correcçao cunha

--- meta3/js/nivel_cinco.js
+++ meta3/js/nivel_cinco.js
@@ -2,10 +2,27 @@
 $(document).ready(function(){
   var $moveable = $('#circle');
     var $preto =$("#overlay");
+
+
+    $preto.css("background-size",$(window).width()*2+"px "+$(window).width()*2+"px "); //mesma largura que altura (igual ao dobro da largura da janela)
+
+    //alterar pos para centrar
+    $preto.css("background-position-x",(-$(window).width()/2)+"px");
+    $preto.css("background-position-y",(-$(window).width()/2)+"px");
+
+
   $(document).mousemove(function(e){
-      $moveable.css({'top': e.pageY-100,'left': e.pageX-100});
-      console.log("tes");
-      $preto.css({"background": "-webkit-radial-gradient(transparent"+ e.pageY+"px, rgba(0,0,0,1) 20px)" });
+      //$moveable.css({'top': e.pageY-100,'left': e.pageX-100});
+      console.log("tes"+e.pageY);
+      //$preto.css("background","-webkit-radial-gradient(transparent 200px, rgba(0,0,0,1) 150px)");
+
+
+      $preto.css("background-size",$(window).width()*2+"px "+$(window).width()*2+"px ");
+
+      //mudar a pos consoante o rato
+      $preto.css("background-position-x",(e.pageX-$(window).width()/2-$(window).width()/2)+"px");
+      $preto.css("background-position-y",(e.pageY-$(window).width()/2-$(window).width()/2)+"px");
+
   });
 });
 
--- meta3/js/nivel_treze.js
+++ meta3/js/nivel_treze.js
@@ -3,13 +3,16 @@ var click=new Array();
 
 $(".bambu").on("click", function(e){
     console.log(".bambu");
-        total++;
-    $(this).attr('class', 'bambuclicked');
-        console.log("total: "+total+" "+e.target);
-        if(total==33){
-     $(this).load("nivel_catorze.html");
-    window.open(" nivel_catorze.html","_self");
-}
+    total++;
+
+    $(this).off("click");
+
+    $(this).addClass('bambuclicked');
+    //$(this).attr('class', 'bambuclicked');
+    console.log("total: "+total+" "+e.target);
+    if(total==33){
+        $(this).load("nivel_catorze.html");
+        window.open(" nivel_catorze.html","_self");
+    }
 
  });
-    
