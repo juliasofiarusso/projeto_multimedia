cdc7cbf11604a50396533dde5574075ff6abe98c
martavasconcelos <martavasconcelos14@gmail.com>
2016-05-23 17:02:15 +0100

    js 1

--- meta3/js/nivel_um.js
+++ meta3/js/nivel_um.js
@@ -8,3 +8,25 @@ $("#bambu").on("click", function(){
     window.open("nivel_dois.html","_self");
 });
 
+ $("#contador").text('Clicks: ' + clicks);
+$("body").on("click", function(){
+    clicks++;
+    $("#contador").text('Clicks: ' + clicks);
+     sessionStorage.setItem ("click", clicks);
+     console.log("clicks: " + clicks);
+});
+
+var timer = setInterval(
+    function() {
+        seconds++;
+        sessionStorage.setItem("seg", seconds);
+        console.log(seconds);
+    }, 1000
+);
+
+
+$("#desistir").on("click",function(){
+            $(this).load("final1.html");
+    window.open("final1.html","_self");
+          
+          });
