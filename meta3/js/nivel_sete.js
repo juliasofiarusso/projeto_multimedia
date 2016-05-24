1583d71a85797e7d67b7b2068d0c194179f9b48d
juliasofiarusso <julia_russo_@hotmail.com>
2016-05-24 23:21:49 +0100

    cenas MARTA

--- meta3/js/nivel_sete.js
+++ meta3/js/nivel_sete.js
@@ -4,7 +4,7 @@ $("#bambu").on("click", function(){
     clicks_bambu++;
     console.log(clicks_bambu);
     if(clicks_bambu==10){
-        $("p").text("Ainda acreditas no que te digo?");
+       $("#p").text("Mas ainda acreditas no que te digo?!");
        
     }
     if (clicks_bambu==11){
