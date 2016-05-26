b4d55101ce35256cb6d8d27ec1cf2181bf379c47
martavasconcelos <martavasconcelos14@gmail.com>
2016-05-26 18:13:13 +0100

    9 random

--- meta3/js/nivel_nove.js
+++ meta3/js/nivel_nove.js
@@ -1,5 +1,6 @@
-
-$("#bambu4").on("click", function(){
+var b=parseInt(Math.random()*5 +1);
+console.log(b);
+$("#bambu"+b).on("click", function(){
     $(this).load("nivel_dez.html");
     window.open("nivel_dez.html","_self");
 });
