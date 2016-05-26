9aeba8566c4ec4f13b0c87b39297dd2ff9b5b8f4
martavasconcelos <martavasconcelos14@gmail.com>
2016-05-26 16:58:29 +0100

    finais

--- meta3/js/nivel_um.js
+++ meta3/js/nivel_um.js
@@ -3,7 +3,7 @@ var seconds=0;
 sessionStorage.setItem ("click", clicks);
 sessionStorage.setItem ("seg", seconds);
 
-$("#bambu").on("click", function(){
+$("bambu").on("click", function(){
     $(this).load("nivel_dois.html");
     window.open("nivel_dois.html","_self");
 });
